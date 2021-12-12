1e10597630ad9e86cd54b5a9b7502d0d64d4d045
savvita <Savc_ea11@student.itstep.org>
2021-12-12 13:37:41 +0200

    add sets

--- Player/c_Player.cpp
+++ Player/c_Player.cpp
@@ -1,1 +1,28 @@
 #include "c_Player.h"
+
+c_Player::c_Player()
+{
+	health = 0;
+	damage = 0;
+}
+
+c_Player::c_Player(int health, int damage):c_Player()
+{
+	if (health >= 0 && health <= 100)
+		this->health = health;
+
+	if (damage >= 0 && damage <= 100)
+		this->damage = damage;
+}
+
+void c_Player::setHealth(int value)
+{
+	if (value >= 0 && value <= 100)
+		this->health = value;
+}
+
+void c_Player::setDamage(int value)
+{
+	if (value >= 0 && value <= 100)
+		this->damage = value;
+}
--- Player/c_Player.h
+++ Player/c_Player.h
@@ -1,8 +1,17 @@
 #pragma once
+
 class c_Player
 {
 private:
 	int health;
 	int damage;
+
+public:
+	c_Player();
+	c_Player(int health, int damage);
+
+	//sets
+	void setHealth(int value);
+	void setDamage(int value);
 };
 
